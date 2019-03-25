099c3d2dfd56fdc3ea1f0f321fc170925c24aa0a
AngieMonroe <ame.monroy@gmail.com>
2019-03-24 22:09:38 -0600

    ajuste enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBVMTIET85G5uN0y04sL2K0NSfj5Smia4k",
+    authDomain: "pyme-d1517.firebaseapp.com",
+    databaseURL: "https://pyme-d1517.firebaseio.com",
+    projectId: "pyme-d1517",
+    storageBucket: "pyme-d1517.appspot.com",
+    messagingSenderId: "672257347483"
+  }
 };
